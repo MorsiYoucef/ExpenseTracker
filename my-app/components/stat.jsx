0ede4ced43cae8103cab84436934e136f3b17bc0
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-08-11 14:32:05 +0100

    new commit

---
+++ my-app/components/stat.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Stat = () => {
+  return (
+    <View className=''>
+      <Text className='text-red-600'>Stat</Text>
+    </View>
+  )
+}
+
+export default Stat
+
+const styles = StyleSheet.create({})
