f316de72a24cfcb1d3a06b6a034e75485d75204c
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-08-12 09:29:45 +0100

    new commit

--- my-app/App.js
+++ my-app/App.js
@@ -3,6 +3,8 @@ import Expenses from './screens/Expenses'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
+import { TouchableOpacity } from 'react-native'
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5'
 
 const Tab = createBottomTabNavigator()
 
@@ -12,6 +14,21 @@ export default function App() {
       <Tab.Navigator
         screenOptions={{
           tabBarStyle: { height: 70 }, // Adjust the height as needed
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#214177', // Change this to your desired background color
+          },
+          headerTintColor: '#fff',
+          headerRight: () => (
+            <TouchableOpacity onPress={() => alert('Icon Pressed!')}>
+              <FontAwesome5
+                name="plus"
+                size={24}
+                color="#BDD8F1"
+                style={{ marginRight: 15 }}
+              />
+            </TouchableOpacity>
+          ),
         }}
       >
         <Tab.Screen
@@ -27,6 +44,7 @@ export default function App() {
                 />
               )
             },
+            title: 'Recent Expenses',
           }}
         />
         <Tab.Screen
@@ -42,6 +60,7 @@ export default function App() {
                 />
               )
             },
+            title: 'All Expenses',
           }}
         />
       </Tab.Navigator>
