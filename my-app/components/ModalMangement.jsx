5558c7ed4239e3f2c8d2984d2d2c7a1d23a5a53d
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-08-14 12:40:46 +0100

    new commit

--- my-app/components/ModalMangement.jsx
+++ my-app/components/ModalMangement.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const ModalMangement = ({ update, add }) => {
@@ -6,8 +6,17 @@ const ModalMangement = ({ update, add }) => {
     <View>
       {update && <Text>Update Mode</Text>}
       {add && (
-        <View>
-          <Text>Add Mode</Text>
+        <View className="flex flex-row gap-5">
+          <TouchableOpacity className=" bg-myBlue_200 p-2">
+            <Text className="text-white text-center text-xl font-bold ">
+              Cancel
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity className=" bg-myBlue_200 p-2 text-center">
+            <Text className="text-white text-center text-xl font-bold">
+              Add
+            </Text>
+          </TouchableOpacity>
         </View>
       )}
     </View>
