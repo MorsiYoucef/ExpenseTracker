dd9eee498bde65504661d7e40f3985619baadebe
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-08-17 10:32:11 +0100

    new commit

--- my-app/components/ModalMangement.jsx
+++ my-app/components/ModalMangement.jsx
@@ -1,22 +1,55 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native'
+import React,{useState} from 'react'
 
 const ModalMangement = ({ update, add }) => {
+  const [title, setTitle] = useState('')
+  const [price, setPrice] = useState('')
+
+  const handleAdd = () => {
+    // Logic to add the item to the list
+    console.log('Title:', title, 'Price:', price)
+  }
   return (
     <View>
       {update && <Text>Update Mode</Text>}
       {add && (
         <View className="flex flex-row gap-5">
-          <TouchableOpacity className=" bg-myBlue_200 p-2">
-            <Text className="text-white text-center text-xl font-bold ">
-              Cancel
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity className=" bg-myBlue_200 p-2 text-center">
-            <Text className="text-white text-center text-xl font-bold">
-              Add
-            </Text>
-          </TouchableOpacity>
+          <View className="flex flex-col gap-5">
+            <TextInput
+              placeholder="Title"
+              value={title}
+              onChangeText={setTitle}
+              className="border p-2"
+            />
+            <TextInput
+              placeholder="Price"
+              value={price}
+              onChangeText={setPrice}
+              keyboardType="numeric"
+              className="border p-2"
+            />
+            <View className="flex flex-row gap-5">
+              <TouchableOpacity className=" bg-myBlue_200 p-2">
+                <Text className="text-white text-center text-xl font-bold ">
+                  Cancel
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                className=" bg-myBlue_200 p-2 text-center"
+                onPress={handleAdd}
+              >
+                <Text className="text-white text-center text-xl font-bold">
+                  Add
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
       )}
     </View>
---
+++ my-app/data/ExpensesData.js
@@ -0,0 +1,5 @@
+import Expenses from "../modal/expenses";
+
+export const EXPENSES = [
+    new Expenses(1, "Groceries", 120),
+]
